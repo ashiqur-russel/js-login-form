a5119fce9e83e5a435cbae966713350332353501
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-18 01:52:59 +0200

    javascript validation for button submit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 // Target all the id and classes
-let id =(id) => document.getElementById(id);
+let id = (id) => document.getElementById(id);
 let classes = (classes) => document.getElementsByClassName(classes);
 
 // store all thr id and classes inside variable
@@ -8,22 +8,34 @@ let username    = id("username"),
     email       = id("email"),
     password    = id("password"),
     form        = id("form"),
-    
-    errorMessage    = classes("error"),
-    successIcon     = classes("success-icon"),
-    failureIcon     = classes("failure-icon");
-// Target the form and add submit ebent listener
+    errorMsg    = classes("error"),
+    successIcon = classes("success-icon"),
+    failureIcon = classes("failure-icon");
 
-form.addEventListener("submit",(e)=>{
-    e.preventDefault();
+// Target the form and add submit ebent listener
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  engine(username, 0, "Username cannot be blank");
+  engine(email, 1, "Email cannot be blank");
+  engine(password, 2, "Password cannot be blank");
 });
 
 // function for doing all validation
 let engine = (id, serial, message) => {
-    if(id.value.trim()===""){
-        errorMessage[serial].innerHTML= message;
+  if (id.value.trim() === "") {
+    errorMsg[serial].innerHTML = message;
+    id.style.border = "2px solid red";
 
-    }else{
-            errorMsg[serial].innerHTML = "";
-    }
-}
+    // icons
+    failureIcon[serial].style.opacity = "1";
+    successIcon[serial].style.opacity = "0";
+  } else {
+    errorMsg[serial].innerHTML = "";
+    id.style.border = "2px solid green";
+
+    // icons
+    failureIcon[serial].style.opacity = "0";
+    successIcon[serial].style.opacity = "1";
+  }
+};
