897f6414d9e1e0bab5c46ad8d7fc279bbd1862dd
ashiqurras <ashiqurrussel2@gmail.com>
2021-10-19 01:21:29 +0200

    Wrap up & formatting

--- index.js
+++ index.js
@@ -14,28 +14,29 @@ let username    = id("username"),
 
 // Target the form and add submit ebent listener
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  engine(username, 0, "Username cannot be blank");
-  engine(email, 1, "Email cannot be blank");
-  engine(password, 2, "Password cannot be blank");
+    engine(username, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    engine(password, 2, "Password cannot be blank");
 });
 
 // function for doing all validation
 let engine = (id, serial, message) => {
-  if (id.value.trim() === "") {
-    errorMsg[serial].innerHTML = message;
-    id.style.border = "2px solid red";
+    if (id.value.trim() === "") {
+        errorMsg[serial].innerHTML = message;
+        id.style.border = "2px solid red";
 
     // icons
-    failureIcon[serial].style.opacity = "1";
-    successIcon[serial].style.opacity = "0";
-  } else {
-    errorMsg[serial].innerHTML = "";
-    id.style.border = "2px solid green";
+        failureIcon[serial].style.opacity = "1";
+        successIcon[serial].style.opacity = "0";
+
+    } else {
+        errorMsg[serial].innerHTML = "";
+        id.style.border = "2px solid green";
 
     // icons
-    failureIcon[serial].style.opacity = "0";
-    successIcon[serial].style.opacity = "1";
-  }
+        failureIcon[serial].style.opacity = "0";
+        successIcon[serial].style.opacity = "1";
+}
 };
